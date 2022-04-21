e101d4e6e87f5b238447549d120cdaaefdaf1afb
RookieCoderrr <zilie.imtatlantique@gmail.com>
2022-04-21 14:47:08 +0800

    start.sh 2

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo shut down existed docker service
 echo you env is $1
-docker stop nestjs
+docker stop  neofura-authservice-nestjs-1
 
-docker container rm nestjs
+docker container rm  neofura-authservice-nestjs-1
 
 docker rmi neofura-authservice_nestjs -f
 docker-compose up -d
